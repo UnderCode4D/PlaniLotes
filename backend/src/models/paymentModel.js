03eed974b6cf373f99d1ff9fa338b95d7e134b68
Frnn4268 <appstutorialesy@gmail.com>
2025-02-23 22:43:58 -0600

    Adding new models

---
+++ backend/src/models/paymentModel.js
@@ -0,0 +1,65 @@
+const { Schema, mongoose } = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+  responsible: {
+    type: String,
+    required: false,
+  },
+  ammount: {
+    type: Number,
+    required: true,
+  },
+  payment_date: {
+    type: Date,
+    required: true,
+  },
+  transaction_type: {
+    type: String,
+    required: true,
+    enum: [ 'Enganche', 'Abono', 'Apartado', 'Anticipo' ],
+  },
+  payment_method: {
+    type: String,
+    required: true,
+    enum: [ 'Deposito', 'Efectivo', 'Cheque' ],
+  },
+  payment_type: {
+    type: String,
+    required: true,
+    enum: [ 'Ingreso', 'Egreso' ],
+  },
+  remaining_payments: {
+    type: Number,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  another: {
+    type: String,
+    required: false,
+  },
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: false,
+  },
+  lot: {
+    type: Schema.Types.ObjectId,
+    ref: 'Lot',
+    required: false,
+  },
+}, { timestamps: true });
+
+paymentSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+// Payment model
+const Payment = mongoose.model('Payment', paymentSchema);
+module.exports = Payment;
